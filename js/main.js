058a7413322accea401e49a04e31c90f8e63b30d
Katehok <a.timin2003@mail.ru>
2025-01-01 23:06:39 +0300

    Changed colors for light and dark themes, saving choosen color theme

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+const LOCAL_STORAGE_COLOR_THEME_NAME = 'KMcolorTheme';
 const HTML_ELEMENT = document.querySelector("html");
 const COLOR_THEME_BTN_ITEMS_ELEMENTS = document.querySelectorAll(".site-theme-toggle-btn .toggle-btn-input");
 
+window.addEventListener('DOMContentLoaded', () => {
+    const THEME = window.localStorage.getItem(LOCAL_STORAGE_COLOR_THEME_NAME);
+    COLOR_THEME_BTN_ITEMS_ELEMENTS.forEach(element => {
+        if (element.value == THEME) element.checked = true;
+        else element.checked = false;
+    });
+    setColorTheme(THEME);
+});
+
 COLOR_THEME_BTN_ITEMS_ELEMENTS.forEach(item => item.addEventListener('click', e => {
+    setColorTheme(e.target.value);
+    window.localStorage.setItem(LOCAL_STORAGE_COLOR_THEME_NAME, e.target.value);
+}));
+
+function setColorTheme(theme = 'light') {
     const CLASS_DARK = "dark";
-    if (e.target.value == CLASS_DARK) {
+    if (theme == CLASS_DARK) {
         if (!HTML_ELEMENT.classList.contains(CLASS_DARK))
             HTML_ELEMENT.classList.add(CLASS_DARK);
     }
     else if (HTML_ELEMENT.classList.contains(CLASS_DARK))
         HTML_ELEMENT.classList.remove(CLASS_DARK);
-}));
+}
